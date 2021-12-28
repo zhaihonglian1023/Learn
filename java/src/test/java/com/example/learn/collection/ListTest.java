19a9a4bc8115ee10e853ec96f8091ef43dc6ceac
zhaihonglian <zhaihonglian@58.com>
2021-12-28 17:40:16 +0800

    更新test

--- java/src/test/java/com/example/learn/collection/ListTest.java
+++ java/src/test/java/com/example/learn/collection/ListTest.java
@@ -1,4 +1,23 @@
 package com.example.learn.collection;
 
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListTest {
+
+    @Test
+    public void test1(){
+        List<Object> list = new ArrayList<Object>(10);
+        list.add(123);
+        list.add(456);
+        list.add(789);
+        list.add("hello");
+        list.add("world");
+        list.add(1,"pdd");
+        System.out.println(list);
+        System.out.println("列表长度:"+list.size());
+        System.out.println(list.stream().getClass());
+    }
 }
--- java/src/test/java/com/example/learn/collection/MapTest.java
+++ java/src/test/java/com/example/learn/collection/MapTest.java
@@ -1,4 +1,20 @@
 package com.example.learn.collection;
 
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapTest {
+
+    @Test
+    public void test1(){
+        Map<String,Object> map = new HashMap<String, Object>(10);
+        map.put("name","tom");
+        map.put("age",20);
+        map.put("hobby","singsong");
+        map.forEach((key,value)->{
+            System.out.println(key+":"+value);
+        });
+    }
 }
