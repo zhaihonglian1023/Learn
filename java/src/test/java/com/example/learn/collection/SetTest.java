6cc0e2ec6f95ccec3bbd9fa03f6a2358278a2cc3
zhaihonglian <zhaihonglian@58.com>
2021-12-30 11:14:50 +0800

    添加set

--- java/src/test/java/com/example/learn/collection/SetTest.java
+++ java/src/test/java/com/example/learn/collection/SetTest.java
@@ -3,6 +3,7 @@ package com.example.learn.collection;
 import org.testng.annotations.Test;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -15,11 +16,24 @@ public class SetTest {
         set.add("hello");
         System.out.println(set);
         System.out.println("------------------");
+
+    }
+    @Test
+    public void test2(){
         Set<Integer> set1 = new TreeSet<>();
         set1.add(123);
         set1.add(879);
         set1.add(333);
         System.out.println(set1);
         System.out.println("------------------");
+    }
+    @Test
+    public void test3(){
+        Set<Object> set = new LinkedHashSet<>();
+        set.add(123);
+        set.add("hello");
+        set.add("world");
+        set.add(345);
+        System.out.println(set);
     }
 }
