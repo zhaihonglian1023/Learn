9c77bf33d8e7489d4b2ef96ddf390879a82c51f8
zhaihonglian <zhaihonglian@58.com>
2021-12-30 11:08:50 +0800

    添加set

--- java/src/test/java/com/example/learn/collection/MapTest.java
+++ java/src/test/java/com/example/learn/collection/MapTest.java
@@ -1,7 +1,6 @@
 package com.example.learn.collection;
 
 import org.testng.annotations.Test;
-
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -10,7 +9,7 @@ public class MapTest {
 
     @Test
     public void test1(){
-        Map<String,Object> map = new HashMap<String, Object>(10);
+        Map<String,Object> map = new HashMap<String,Object>(10);
         map.put("name","tom");
         map.put("age",20);
         map.put("hobby","singsong");
@@ -18,10 +17,16 @@ public class MapTest {
             System.out.println(key+":"+value);
         });
         System.out.println("size:"+map.size());
-        System.out.println("-------------------");
+        System.out.println("-----------------------------");
         Iterator<Object> iterator = map.values().iterator();
         while (iterator.hasNext()){
             System.out.println(iterator.next());
         }
+    }
+
+    @Test
+    public void test2(){
+        Map<String,Object> map = new HashMap<>();
+        System.out.println(map);
     }
 }
--- java/src/test/java/com/example/learn/collection/SetTest.java
+++ java/src/test/java/com/example/learn/collection/SetTest.java
@@ -1,4 +1,25 @@
 package com.example.learn.collection;
 
+import org.testng.annotations.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class SetTest {
+    @Test
+    public void test1(){
+        Set<Object> set = new HashSet<>();
+        set.add(123);
+        set.add("234");
+        set.add("hello");
+        System.out.println(set);
+        System.out.println("------------------");
+        Set<Integer> set1 = new TreeSet<>();
+        set1.add(123);
+        set1.add(879);
+        set1.add(333);
+        System.out.println(set1);
+        System.out.println("------------------");
+    }
 }
